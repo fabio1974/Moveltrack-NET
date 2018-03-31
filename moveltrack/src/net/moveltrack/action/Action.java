a395eba8c30d9420ae51317ab4dc50b53de1a099
fabio1974 <fabio.barros1974@gmail.com>
2018-03-31 15:04:09 -0300

    commit mudança só no java

--- moveltrack/src/net/moveltrack/action/Action.java
+++ moveltrack/src/net/moveltrack/action/Action.java
@@ -6,6 +6,7 @@ import javax.inject.Named;
 public class Action {
 	public static final String SHOW = "SHOW";
 	public static final String UPDATE = "UPDATE";
+	
 	public static final String INSERT = "INSERT";
 	public static final String DELETE = "DELETE";
 	public static final String SMARTINSERT = "SMARTINSERT";
